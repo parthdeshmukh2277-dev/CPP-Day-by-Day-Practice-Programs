98c5bd1937abcc23add2e35a5c4bccd05ef1618e
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-11-15 16:01:39 +0530

    CPP practice program for addition of two Integer

---
+++ Additionexample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,sum;
+    cout<<"Enter First Number\n";
+    cin>>x;
+    cout<<"Enter Second Number\n";
+    cin>>y;
+    sum = x + y;
+    cout<<"Sum of Two Number is\n"<<sum;
+    return 0;
+}
