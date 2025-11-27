d14936c1715596f3cdfc4a1ffdf36feab5937ec0
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-11-27 11:38:23 +0530

    CPP Practice program for Finding out Area of Circle

---
+++ Areaofcircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    float pi = 3.14159265359, r , Area;
+cout<< "Enter the Radius of Circle :\n";
+cin>>r;
+Area = pi*r*r;
+cout<< "Area of Circle is :"<<Area;
+return 0;
+}
