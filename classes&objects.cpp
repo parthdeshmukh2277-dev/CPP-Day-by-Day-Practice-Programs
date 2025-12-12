58a9d0645a5dc8677c8b112900ce6f880ffb9c0f
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-12 13:18:57 +0530

    learn-cpp-classes-objects

---
+++ classes&objects.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Car {
+public:
+    string brand;
+    int year;
+
+    void start() {
+        cout << brand << " is starting..." << endl;
+    }
+};
+int main() {
+    Car c1;
+    c1.brand = "Toyota";
+    c1.year = 2020;
+
+    Car c2;
+    c2.brand = "Honda";
+    c2.year = 2018;
+
+    // Using object methods
+    c1.start();
+    c2.start();
+
+    return 0;
+}
