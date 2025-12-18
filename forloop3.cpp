1eab135b65a417ff92ec81fe7c4730cb481a6ff2
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-18 11:15:31 +0530

    Implementing C++ program to print even numbers up to n using for loop and conditionals statement

---
+++ forloop3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter n = ";
+    cin >> n;
+    cout << "Even numbers are : ";
+for (int i = 0; i <= n; i++) {
+    if (i%2==0) {
+    cout << i << ",";
+    }
+}
+return 0;
+}
