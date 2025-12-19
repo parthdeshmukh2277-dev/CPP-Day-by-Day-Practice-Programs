abcfce4b590fa7e00aa613b7b83cbd4041eec4f0
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-19 12:20:16 +0530

    printing GP 1,2,4,8,16,32.........'n'using for loop without using Arithmatic progression formula

---
+++ Loops/Geometricprogression.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    // print GP 1,2,4,8,16,32.........'n'
+    int a = 1;
+    for(int i = 1; i <= n; i++) {
+        cout << a << ",";
+        a = a * 2;
+    }
+    return 0;
+}
