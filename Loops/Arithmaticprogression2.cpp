59c0ef5606443ac7dfc5574ec57a8f7495ccc62a
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-19 12:00:45 +0530

    printing AP 4,7,10,13.........'n'using for loop without using Arithmatic progression formula

---
+++ Loops/Arithmaticprogression2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    // print AP 4,7,10,13.........'n'
+    int a = 4;
+    for(int i = 1; i <= n; i++) {
+        cout << a << ",";
+        a = a + 3;
+    }
+    return 0;
+}
