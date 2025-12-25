fed1abfdc53d7fa747d49befebcc20baf0431ff1
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-26 01:28:54 +0530

    Practicing Permutation and Combination using loops in cpp

--- Loops/combination.cpp
+++ Loops/combination.cpp
@@ -13,11 +13,11 @@ for (int i=1; i<=n; i++) {
 }
 int b = 1;
 for (int i=1; i<=r; i++) {
-    b = b*i;
+    b = b*i; //this is for r!
 }
 int c = 1;
 for (int i=1; i<=n-r; i++) {
-    c = c*i;
+    c = c*i; //this is for (n-r)!
 }
 cout << a/(b*c); //i.e n!/r!(n-r)!
 }
--- Loops/permutation.cpp
+++ Loops/permutation.cpp
@@ -9,12 +9,12 @@ int main () {
     cin >> r;
     int a = 1;
     for(int i = 1; i<=n; i++){
-        a = a*i;
+        a = a*i; //this is for n!
     }
     int b = 1;
     for(int i = 1; i<=n-r; i++){
-        b = b*i;
+        b = b*i; //this is for (n-r)!
     }
-    cout << a/b;
+    cout << a/b; //this is for n!/(n-r)!
     return 0;
 }
