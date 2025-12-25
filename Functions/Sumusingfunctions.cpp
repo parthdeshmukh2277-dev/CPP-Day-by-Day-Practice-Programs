7d737a64920168bc132b36ee3c8d5359a38cb128
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-26 00:15:58 +0530

    Using functions for finding sum of 2 numbers

--- Functions/Sumusingfunctions.cpp
+++ Functions/Sumusingfunctions.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 using namespace std;
 void sum (int a, int b) {
-cout << "Enter first number : ";
-cin >> a; 
-cout << "Enter second number : ";
-cin >> b; 
 cout << a + b;
 }
 int main () {
-    sum (1,1);
+    sum (5,8);
 }
