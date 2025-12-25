a78894c1a38cc3dd53c42ea803a03b785a9b5ac8
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-25 20:12:54 +0530

    Creating multiple functions and calling them in main finction to print them

---
+++ Functions/Multiplefunctions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void india(){
+    cout << "You are in India." << "\n";
+}
+void usa(){
+    cout << "You are in USA." << "\n";
+}
+int main () {
+    india ();
+    usa ();
+}
