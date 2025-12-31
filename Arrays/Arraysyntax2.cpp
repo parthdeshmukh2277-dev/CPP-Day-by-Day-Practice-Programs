44db2026b04a96aa1b20ccfb1c08009a6fc278f9
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2025-12-31 19:23:06 +0530

    Learning Basic Syntax of arrays

---
+++ Arrays/Arraysyntax2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int arr[5]; //Declartion of array
+    arr [0] = 8; //Set the array
+    arr [1] = 2;
+    arr [2] = 3;
+    arr [3] = 7;
+    arr [4] = 4;
+    int n;
+    for (int i = 0; i<=4; i++)
+    cout << arr [i] << " ";
+}
