0f72fe6a3e34467f4ebf49468c0263e9c8f7a0dd
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-02 12:50:31 +0530

    passing an array to the function term known as pass by reference

---
+++ Arrays/PassingArraytoFunction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void change (int arr []){
+arr[0] = 9;
+arr[4] = 8;
+}
+int main () {
+int arr [] = {1,2,3,4,5};
+for (int i =0; i<=4; i++){
+    cout << arr[i] << " ";
+}
+cout << "\n";
+change (arr);
+for (int i =0; i<=4; i++){
+    cout << arr[i] << " ";
+}
+return 0;
+}
