61ad93df48eee44475f9bc75d7120aa0ca27bead
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-14 00:24:30 +0530

    Finding total digits of given number using to_string

---
+++ Strings/totaldigitofnumber.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    int x = 2344332;
+    string s = to_string(x);
+    cout << s.length(); //output number will be a string
+}
