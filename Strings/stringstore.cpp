498a8a691318eb799a12d7a816737848b16a6c69
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-13 16:03:52 +0530

    Storing string and learning string concept

--- Strings/stringstore.cpp
+++ Strings/stringstore.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-int main() {
-    string str;
-    cout << "Enter a string : ";
-    cin >> str;
+int main () {
+    string str = "Parth Deshmukh";
     cout << str;
 }
