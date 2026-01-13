256e75427f4482fd82e53265a883f3ee99c5935d
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-13 21:48:14 +0530

    Learning about clear in strings

---
+++ Strings/clear.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string s = "Parth";
+    cout << s << "\n" << "Length of string is : " << s.length() << "\n";
+    s.clear();
+    cout << s << "\n" << "Length of string is : " << s.length() << "\n";
+}
