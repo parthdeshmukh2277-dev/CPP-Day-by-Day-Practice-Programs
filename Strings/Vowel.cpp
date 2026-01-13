dabfdb36686953a223b5013d2f784054c4c966c2
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-13 21:12:25 +0530

    finding out total vowels present ina string

--- Strings/Vowel.cpp
+++ Strings/Vowel.cpp
@@ -7,7 +7,7 @@ int main () {
     getline(cin,str);
     int count = 0;
     for (int i = 0; i < str.length(); i++) {
-        if (str[i]== 'a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+        if (str[i]== 'a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]== 'A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
         count ++;  
     }
     cout << "Total vovels in string are : " << count;
