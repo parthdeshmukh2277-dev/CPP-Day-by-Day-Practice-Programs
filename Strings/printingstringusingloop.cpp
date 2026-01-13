cdd47864c1a0ef0bb58aa7982342c91732d022ca
parthdeshmukh2277-dev <parthdeshmukh2277@gmail.com>
2026-01-13 20:37:53 +0530

    Printing the string using loop

---
+++ Strings/printingstringusingloop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string str = "Parth Deshmukh";
+    int n = str.length();
+    for(int i=0; i<n; i++){
+        cout << str[i];
+    }
+}
